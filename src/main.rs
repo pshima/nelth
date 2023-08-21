24819a58477316c75d6a34dbce5bcb41e5ed2803
Pete Shima <me@peteshima.com>
2023-08-21 09:59:41 -0700

    Add https support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use hyper::Client;
+use hyper_tls::HttpsConnector;
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let client = Client::new();
-    let uri = "http://google.com".parse()?;
+    let https = HttpsConnector::new();
+    let client = Client::builder()
+    .build::<_, hyper::Body>(https);
+    let uri = "https://google.com".parse()?;
     let resp = client.get(uri).await?;
     println!("Response: {}", resp.status());
     println!("Headers: {:#?}\n", resp.headers());
